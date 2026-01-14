1a0f9c0300eada02aaf410c81e72d2d6755cfc7a
Lucas Bittencourt <lucasbt@gmail.com>
2026-01-14 00:11:53 -0300

    fix: install deps

--- modules/dev-tools.sh
+++ modules/dev-tools.sh
@@ -211,7 +211,7 @@ EOF
     log_success "Docker installed and configured."
 
     log_section "Installing Modern CLI Tools"
-    dnf_install btop neovim vim-enhanced wget perf htop iotop 
+    dnf_install btop neovim vim-enhanced wget perf htop iotop sudo dnf install --setopt=install_weak_deps=False 
     log_success "Modern CLI tools installed."
 
     log_section "Installing Other Programming Languages"
